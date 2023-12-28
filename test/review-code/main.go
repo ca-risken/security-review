34216063a6c85b49141c4cf4e3af118331fed931
gassara-kys <ogasawara_kiyoshi@cybernagent>
2023-12-28 10:49:29 +0900

    fix: review code

--- test/review-code/main.go
+++ test/review-code/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Require argument")
+		os.Exit(1)
+	}
+
+	if err := osCmdInjection(args[0]); err != nil {
+		fmt.Printf("Failed to exec cmd: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func osCmdInjection(userInput string) error {
+	cmd := exec.Command(userInput) // OS command injection
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	return cmd.Run()
 }
